835806948f5e2b769922d9e2c0089a729d68c8ad
Kathleen Lee <kl2926@columbia.edu>
2018-02-15 18:03:04 -0500

    Part 3 revised

--- hw2_group6_sh3264_kl2926_vss2113/part3.py
+++ hw2_group6_sh3264_kl2926_vss2113/part3.py
@@ -1,18 +1,28 @@
-#!/user/bin/env python 
+#!/usr/bin/env python
+
+from geometry_msgs.msg import Pose
 
 from graspit_commander import GraspitCommander 
 gc = GraspitCommander() 
 
 ROS_SERVICE_TIMEOUT = 1
 
-#Refresh World 
+# Refresh world  
 gc.clearWorld()
 
-#Load PlannerMug environment and grasp 
-gc.loadWorld("plannerMug")
-gc.approachToContact()
-gc.autoGrasp()
+gc.importRobot('fetch_gripper')
+gc.autoOpen()
 
-#Print position 
+cupPose = Pose()
+cupPose.position.x = 0.05
+cupPose.position.y = 0
+cupPose.position.z = 0
+cupPose.orientation.x = 1.6
+cupPose.orientation.y = -0.3
+cupPose.orientation.z = 0.1
+cupPose.orientation.w = 1.0
+gc.importGraspableBody('mug', cupPose)
+gc.findInitialContact()
+gc.autoGrasp()
 pose = gc.getRobot(0)
 print(pose)
