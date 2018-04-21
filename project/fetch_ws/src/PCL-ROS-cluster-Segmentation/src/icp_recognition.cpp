4d2671b0834ade60da7a267b57fb8acce589cd70
Kathleen Lee <kl2926@columbia.edu>
2018-04-21 18:59:48 -0400

    update icp_recognition and CMakeLists

--- project/fetch_ws/src/PCL-ROS-cluster-Segmentation/src/icp_recognition.cpp
+++ project/fetch_ws/src/PCL-ROS-cluster-Segmentation/src/icp_recognition.cpp
@@ -11,20 +11,26 @@ Author: Sean Cassero
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud_conversion.h>
-
 #include <pcl_conversions/pcl_conversions.h>
-
 #include <iostream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
-#include <iostream>
-#include <sensor_msgs/PointCloud2.h>
 #include <obj_recognition/SegmentedClustersArray.h>
 #include <pcl/io/vtk_lib_io.h>
 #include <pcl/io/ply_io.h>
-#include <Eigen/Core>
 #include <pcl/conversions.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <eigen_conversions/eigen_msg.h>
+
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseStamped.h"
+#include <tf/tf.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <tf2/convert.h>
+#include "tf_conversions/tf_eigen.h"
 
 class icp_recognition {
 
@@ -58,7 +64,7 @@ void icp_recognition::cluster_cb (const obj_recognition::SegmentedClustersArray&
                   << std::endl;
 
 	// Convert candidate meshes to cloud
-        const std::string meshFileName = "/home/kathleen/humanoid_robotics/project/fetch_ws/src/PCL-ROS-cluster-Segmentation/models/soda_can/meshes/soda_can.ply";
+        const std::string meshFileName = "/home/kathleen/humanoid_robotics/project/fetch_ws/src/PCL-ROS-cluster-Segmentation/models/bowl/meshes/bowl.ply";
 
 	pcl::PolygonMesh objectMesh;
 	pcl::io::loadPolygonFilePLY(meshFileName, objectMesh);
@@ -78,17 +84,42 @@ void icp_recognition::cluster_cb (const obj_recognition::SegmentedClustersArray&
         icp.setInputTarget(cloud_out);
         pcl::PointCloud<pcl::PointXYZ> Final;
         icp.align(Final);
-        std::cout << "has converged:" << icp.hasConverged() << " score: " <<
+        std::cout << " has converged:" << icp.hasConverged() << " score: " <<
                   icp.getFitnessScore() << std::endl;
         std::cout << icp.getFinalTransformation() << std::endl;
 	Eigen::Matrix4f trans = icp.getFinalTransformation();
 
+	// Convert Matrix 4f to Affine 3d 
+	Eigen::Matrix4d m4 = trans.template cast<double>();
+	Eigen::Matrix3d m3 = Eigen::Matrix3d::Identity();
+	
+	for i = 1:3 {
+		for j = 1:3 {
+			m3(i,j) = m4(i,j);
+		}
+	}
+	
+	// Convert Matrix4d to Affine3d 
+	//Eigen::Affine3d affine(md);
+	//tf::Transform transform;
+	//tf::transformEigenToTF(affine, transform); 
+
+	// Convert Affine3d to geometry_msgs
+	//geometry_msgs::Pose pose;
+	//tf::transformTFToMsg(transform, ts);
+	//tf::poseEigenToMsg(affine,pose);
+	//tf2::toMsg(affine);
+
+	//std::cout << "Pose of object: " << pose << std::endl;
+	
 	/* 
 	loadPolygonFilePLY - load list of objects as point cloud 
 	To do: 
 	- For every cluster - compare cluster and all meshes and find the best match
 	- Once found, then add to a list of objects found and then later after the for loop, we want to publish the list of objects constantly to a topic. 
-	- 
+	- Debug: 
+	- Publish new topic to Rviz and check whether it is seeing the correct thing 
+	
 	*/ 
     }
 }
