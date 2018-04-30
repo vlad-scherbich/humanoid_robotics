4cf6ac897742c28b6a05a989f0aff942994684d6
Vlad Scherbich <vss2113@columbia.edu>
2018-04-29 20:39:46 -0400

    adding code to open / close Fetch gripper

---
+++ project/fetch_ws/open_close_gripper.py
@@ -0,0 +1,17 @@
+import rospy
+import moveit_commander
+
+
+rospy.init_node('temp')
+move_group = moveit_commander.MoveGroupCommander('gripper')
+
+# open gripper
+move_group.set_named_target('open')
+open_plan = move_group.plan()
+move_group.execute(open_plan)
+
+# close gripper
+move_group.set_named_target('close')
+close_plan = move_group.plan()
+move_group.execute(close_plan)
+
